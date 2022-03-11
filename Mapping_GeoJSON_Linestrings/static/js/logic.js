f4eed2a29be795a3393ab5b7a6d0e9ecc8be0056
Silvia Eiden <im.georgina@gmail.com>
2022-03-10 20:26:12 -0500

    new files

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -76,7 +76,7 @@ console.log("working");
 //});
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -94,35 +94,37 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layers: [streets]
+    center: [44.0, -80.0],
+    zoom: 3,
+    layers: [light]
 });  
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/SilviaEiden/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/SilviaEiden/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
 
-// // Grabbing our GeoJSON data using onEachFeature() Function.
-// L.geoJSON(airportData, {
-//     // We turn each feature into a marker on the map.
-//     onEachFeature: function(feature, layer) {
-//       console.log(layer);
-//       layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
-//     }
-//   }).addTo(map);  
+// Grabbing our GeoJSON data using onEachFeature() Function.
+L.geoJSON(data, {
+    color: "#ffffa1",
+    weight: 2,
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2> <hr> <h2>Destination: " + feature.properties.dst + "</h2>");
+    }
+  }).addTo(map);  
 });
